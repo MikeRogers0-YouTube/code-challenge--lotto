ca583f6150d686c9b4d9d79ebd38696918173c28
Mike Rogers <me@mikerogers.io>
2019-02-06 17:39:59 +0000

    Adding initial challenge for this week - enjoy building a lottery system

---
+++ README.md
@@ -0,0 +1,23 @@
+# Code Challenge
+
+We're going to make a lottery game! Where the user can define a set of numbers, and play a virtual lottery.
+
+In our lottery, we're going to use the numbers 1 through 19, and the user only needs to select 3 numbers to win.
+
+I've setup a `lotto.js` file to get us started. You should be able to copy and paste the file into your browsers console to see it working.
+
+## Tasks
+
+### Task 1: Pick your winning numbers
+
+In the lotto class, select 3 numbers which are your winning numbers.
+
+### Task 2: Let users input their chosen numbers
+
+We want users to pick their numbers like:
+
+    lotto.setChosenNumbers([3,5,6]);
+
+### Task 3: When the users chosen numbers match the ones picked, tell them they're a winner
+
+The final step! Make it so when the users chosen numbers match our winning numbers, we tell them about it.
---
+++ lotto.js
@@ -0,0 +1,50 @@
+// This method lets us call [1,2,3].shuffle and get [3,2,1]
+Array.prototype.shuffle = function () {
+  var m = this.length;
+  while (m) {
+    let i = Math.floor(Math.random() * m--);
+    [this[m], this[i]] = [this[i], this[m]];
+  }
+  return this;
+}
+
+// Our lotto class
+function Lotto() {
+  // This'll give us an array of numbers from 0 to 19.
+  var balls = Array.from({length: 19}, function (v, k) { return k+1 });
+  var chosenNumbers = []
+
+  function shuffleBalls(){
+    balls.shuffle();
+  };
+
+  function setChosenNumbers(newChosenNumbers){
+    console.log('newChosenNumbers: ', newChosenNumbers);
+    // TODO: We need to set the chosenNumbers variable, with the value of newChosenNumbers
+  };
+
+  function play() {
+    console.log( 'Balls: ', balls)
+    shuffleBalls();
+    console.log( 'Shuffled Balls: ', balls)
+
+    console.log('Chosen Numbers', chosenNumbers)
+
+    // TODO: We want to select 3 winning balls for our lottery. 
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+    // [].slice could be the way to do it.
+    console.log('Winning Numbers', 'TODO')
+
+    // TODO: Finally, if the user is a winner, tell them.
+  };
+
+  // These are our public methods, which we'll allow users to interact with.
+  return {
+    setChosenNumbers,
+    play
+  };
+}
+
+var lotto = Lotto();
+lotto.setChosenNumbers([3,5,6]);
+lotto.play();
